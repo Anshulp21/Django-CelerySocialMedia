91a020d655a3b28c1bcc8ee26fa95c536f11db68
Anshulp21 <anshulp490@gmail.com>
2024-10-22 12:00:52 +0530

    changes in recipes

--- recipe_platform/recipes/apps.py
+++ recipe_platform/recipes/apps.py
@@ -1,27 +1,14 @@
 
-# from django.apps import AppConfig
-
-# class RecipesConfig(AppConfig):
-#     name = 'recipes'
-
-#     def ready(self):
-#         # No database queries here
-#         pass
-
-
-
-
 from django.apps import AppConfig
-from django.core.management import call_command
 
 class RecipesConfig(AppConfig):
     name = 'recipes'
 
     def ready(self):
-        # Automatically run setup_tasks when the server starts
-        try:
-            call_command('setup_tasks')
-        except Exception as e:
-            # Handle any errors, you can log this or print the error
-            print(f"Error running setup_tasks: {e}")
+        # No database queries here
+        pass
+
+
+
+
 
